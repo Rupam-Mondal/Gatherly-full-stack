20b66e72276e2bea0305996ed2528b031962c830
Rupam <rupammondal564@gmail.com>
2024-11-10 08:06:00 +0530

    Status code added

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -1,10 +1,11 @@
 import { signinService, signupService } from "../service/userService.js";
+import { StatusCodes } from 'http-status-codes';
 
 export async function signupController(req , res){
     try {
         const userObject = req.body;
         const response = await signupService(userObject);
-        return res.json({
+        return res.status(StatusCodes.CREATED).json({
             success:true,
             message:"Signed up successfully",
             data:response
@@ -22,7 +23,7 @@ export async function signinController(req , res){
     try {
         const userObject = req.body;
         const response = await signinService(userObject);
-        return res.json({
+        return res.status(StatusCodes.OK).json({
             success: true,
             message: "Signin up successfully",
             data: response
